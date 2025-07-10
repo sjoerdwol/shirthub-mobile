1fcbf951148528d0277e0b0274c5ef765fc21c96
sjoerdwol <sjoerd@woltersonline.net>
2025-07-10 20:00:03 +0200

    changed StatBox background color

--- components/home/statBox.tsx
+++ components/home/statBox.tsx
@@ -4,7 +4,7 @@ export default function StatBox({ title, value, size = 'small' }: StatBoxProps) 
   const boxWidth = size === 'large' ? 'w-full' : 'w-[48%]';
 
   return (
-    <View className={`bg-dark-secondary rounded-2xl p-6 mb-4 ${boxWidth} justify-between h-30`}>
+    <View className={`bg-dark-background-250 rounded-2xl p-6 mb-4 ${boxWidth} justify-between h-30`}>
       <Text className="text-dark-text-400 text-lg font-medium mb-2">{title}</Text>
       <Text className="text-dark-text-400 text-3xl font-bold">${value}</Text>
     </View>
